f8a912b51fd05dc3b4c43ec8136e859498cef6d2
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2022-06-30 13:22:04 +0600

    Changes in the full project

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,8 +2,8 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Admin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
-use App\Http\Controllers\Admin\AdminController;
 
 class Kernel extends HttpKernel
 {
@@ -17,7 +17,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Fruitcake\Cors\HandleCors::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin' => \App\Http\Middleware\Admin::class
+        'admin' => Admin::class
     ];
 }
